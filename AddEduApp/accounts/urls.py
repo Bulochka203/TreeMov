e2be52452f7d4c51dd925e2f6a12ca649446a98d
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2025-02-09 19:24:26 +0500

    Update urls.py

--- AddEduApp/accounts/urls.py
+++ AddEduApp/accounts/urls.py
@@ -5,7 +5,11 @@ app_name = "accounts"
 
 urlpatterns = [
     path('', views.redirect_to, name='redirect'),
+    path("entrance/", views.Entrance, name="entrance"),
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("signin/", views.SignInView.as_view(), name="signin"),
     path("signout/", views.signout, name="signout"),
+    path("teacher-code/", views.TeacherCodeView.as_view(), name="teacherCode"),
+    path("create-teacher-code/", views.CreateTeacherCodeView.as_view(),
+         name="create_teacher_code"),
 ]
